f17ee691c1780b92f02aadbe0c58ff29d82d86b1
ypiolet <ypiolet@lana>
2013-03-19 01:56:37 +0100

    Oh yes, outputs and comments.

--- install/setup_linux.sh
+++ install/setup_linux.sh
@@ -6,6 +6,7 @@
 S_PATH="/home/$USER/.schematics"
 
 # Backup
+# TODO: Check if files exist before doing anything stupid
 mkdir ~/.back
 unlink ~/.vimrc
 unlink ~/.vim/
@@ -15,4 +16,15 @@ mv ~/.vim ~/.back
 # Make links
 ln -s "$S_PATH/vim/vimrc" "/home/$USER/.vimrc"
 ln -s "$S_PATH/vim" "/home/$USER/.vim"
+# We love links
+
+# Install these sexy stuff I would never make on my own
+
+# Vundle, Bundle gesture for vim
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+
+# I should tell you something
+echo "Done."
+echo
+echo "Please run vim and type :BundleInstall to install necessary bundles."
+
